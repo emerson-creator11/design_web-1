fffbb09466621518bdfa349cc35145d1aa631b37
emerson-creator11 <c.emerson@escolar.ifrn.edu.br>
2026-09-18 14:04:04 -0300

    colocando inormações

---
+++ js/app1.js
@@ -0,0 +1,8 @@
+const amigos = ["Eduardo", "Iego", "Ravy" ];
+tam = amigos.length;
+
+console.log(`Arranjo ${amigos}`);
+console.log(`tamanho ${tam}`);
+console.log(`primeiro ${amigos[0]}`);
+console.log(`último ${amigos[tam - 1]}`);
+
