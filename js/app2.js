31dcee04f017a6c8f5f52052d0a376d1afe05f7f
emerson-creator11 <c.emerson@escolar.ifrn.edu.br>
2026-09-23 17:44:28 -0300

    criando cod do app.2 e corrigindo html

--- js/app2.js
+++ js/app2.js
@@ -4,9 +4,24 @@ const cadastro = document.getElementById("cadastro");
 const nome = cadastro.nome;
 const nasc = cadastro.nasc;
 const whatsapp = cadastro.whatsapp;
+const lista = document.getElementById("lista");
 
 cadastro.addEventListener("submit", function(e){
     e.preventDefault();
     let item = [nome.value, nasc.value, whatsapp.value];
     amigos.unshift(item);
-})
+    cadastro.reset();
+    exibirLista();
+
+});
+
+function exibirLista(){
+    let itens = "";
+    for(let i = 0; i < amigos.length; i++){
+        let item = amigos[i];
+        let li = `<li>${item[0]} | ${item[1]} | ${item[2]} `;
+        itens = itens + li;
+
+    }
+    lista.innerHTML = itens;
+}
